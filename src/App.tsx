daf1dcf72f99eedaecb3a8e688e917280e5432d9
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-05-31 11:16:31 +0300

    Add 're-importing' index.ts to shorten imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { ReactComponent as AlignCenterIcon } from "./assets/align-center.svg";
 import { ReactComponent as AlignJustifyIcon } from "./assets/align-justify.svg";
 import { ReactComponent as AlignRightIcon } from "./assets/align-left.svg";
-import { Icon } from "./icon/Icon";
+import { Icon } from "./icon";
 import { AddIcon } from "./individual-icon-components/AddIcon";
 import { AddressBookIcon } from "./individual-icon-components/AddressBookIcon";
 import { AlarmClockIcon } from "./individual-icon-components/AlarmClockIcon";
@@ -32,7 +32,7 @@ export const App: FC = () => {
         <div className="icons-container">
           <Icon icon="AlarmClock" />
           <Icon icon="AddressBook" />
-          <Icon icon="Add"/>
+          <Icon icon="Add" />
         </div>
       </div>
     </div>
